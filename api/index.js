a54315d8704c055386b48e9d824d09d0c9db6aa1
manudiiez <manudiiez123@gmail.com>
2022-09-27 23:39:32 -0300

    error file 42:29

---
+++ api/index.js
@@ -0,0 +1,59 @@
+import express from "express"
+import dotenv from 'dotenv'
+import mongoose from "mongoose"
+/* ---------------------------------- RUTAS --------------------------------- */
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
+const app = express()
+dotenv.config()
+
+// CONEXION A MONGODB
+const connect = async ( ) => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log('Connected to mongoDB')
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Si esta desconectada devuelve
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoDB disconnected!!')
+})
+// Si esta conectada devuelve
+mongoose.connection.on('connected', () => {
+    console.log('mongoDB connected!!')
+})
+
+
+// middlewares: son las rutas
+
+// Para poder usar body (json)
+app.use(express.json())
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+// Next le dice a la aplicacion que se ejecute el siguiente middleware.
+// Como no tiene ruta este seria el siguiente middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || 'Something went wrong!'
+    return res.status(500).json({
+        succest: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
+app.listen(8800, () => {
+    connect()
+    console.log('Connected to backend!')
+})
